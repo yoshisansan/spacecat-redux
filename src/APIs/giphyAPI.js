1893fe96e5b21cd2568a5d8a41c58c7a73a13309
Akifumi <kusacurrency@gmail.com>
2019-04-24 13:15:37 +0700

    add-firebase

--- src/APIs/giphyAPI.js
+++ src/APIs/giphyAPI.js
@@ -3,8 +3,9 @@ import axios from "axios";
 const giphyAPI = () => {
   const search = "cat space";
   const key = "P8jSOBEO7BzFdCudvpCKNzqqwIJXNsrY";
-  const limit = 17;
+  const limit = 15;
   const url = `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${key}&limit=${limit}`;
+
   // promise オブジェクトが return されるようにする
   return axios.get(url);
 };
